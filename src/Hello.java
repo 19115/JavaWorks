e23d93c3f4ec7d21656879329927232b85697eea
19115 <liliankibunja60@gmail.com>
2025-01-21 15:47:45 +0300

    my first upload

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+/* simple hello world code
+ outputs Hello world!
+ :::Lilian Kibunja
+ CT101/G/19115/23
+ */
+public class Hello {// the file name is Hello
+    public static void main(String[]args){//the main function
+        System.out.println("Hello world!");
+    }
+}
