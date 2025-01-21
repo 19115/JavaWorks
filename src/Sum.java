0c3af1237add768b3e31bdd6fa315182a5a04be4
19115 <liliankibunja60@gmail.com>
2025-01-21 16:22:35 +0300

    second upload

---
+++ src/Sum.java
@@ -0,0 +1,16 @@
+/* simple summation java code
+ coded by: Lilian Kibunja
+ CT101/G/19115/23
+ Bsc Computer science
+ */
+public class Sum {
+    public static void main (String[]args){
+        System.out.println("lets find the sum of the two integers");
+        int a=30,b=80;//variable declaration and initialization
+        int sum=a+b;//initialization of the sum
+        System.out.println("the sum of "+ a + " and "+ b +" adds up to: "+sum);
+        // + operator is used for string concatenation
+        //the values of  variable a and b are converted to string and concatenated
+
+    }
+}
